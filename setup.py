34bdb92b14c120bcf0b3760e89b611bfcae0631e
Yuuki Ishizaka <s23c1011jk@s.chibakoudai.jp>
2025-01-05 20:24:19 +0900

    途中経過

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
-from setuptools import find_packages, setup
-import os                  
-from glob import glob 
+from setuptools import setup
+import os
+from glob import glob
+
 package_name = 'mypkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],  # 単一パッケージの場合は明示的に指定
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('launch/*.launch.py') or []),  # launch ファイルが空でもエラーにならないように
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='yuuki',
-    maintainer_email='yuuki@todo.todo',
-    description='a package for practice',
+    maintainer_email='yuuki@example.com',  # プレースホルダーを実際のメールアドレスに変更
+    description='A package for practice',
     license='BSD-3-Clause',
-    tests_require=['pytest'],
+    tests_require=['pytest', 'ament_pytest'],  # 追加のテスト依存関係
     entry_points={
         'console_scripts': [
-             'talker = mypkg.talker:main',
-             'listener = mypkg.listener:main',
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
+
